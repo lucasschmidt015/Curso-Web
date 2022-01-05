9bc5546f03a6b11c660f824d0ec7c50e77c7ccf2
Lucas Schmidt <lucas.schmidt015@gmail.com>
2022-01-05 12:48:17 -0300

    atualizações

---
+++ Fundamentos/UsandoVar.js
@@ -0,0 +1,9 @@
+function teste(){
+    var local = "junto" 
+}
+
+teste()
+console.log(local)
+
+//Em var o escopo ou é global ou cria no escopo de função 
+
---
+++ Fundamentos/usandoLet1.js
@@ -0,0 +1,12 @@
+var numero = 1
+
+{
+    let numero = 2
+    console.log("dentro =", numero)
+}
+
+console.log("Fora =", numero)
+
+//com let o escopo vai se restringir ao bloco em que esta declarado
+//var vai estar em um escopo global a menos que seja declarado em função
+
