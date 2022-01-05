e30518ff8da5d37b85574642f408b21f37cd2b9c
Lucas Schmidt <lucas.schmidt015@gmail.com>
2022-01-05 12:55:45 -0300

    diferenças de var e let

---
+++ Fundamentos/loopLet.js
@@ -0,0 +1,6 @@
+for(let i = 0;i < 10; i++){
+    console.log(i)
+}
+console.log(i)
+
+//como let tem escopo de bloco ele não roda fora do bloco em que foi declarado
---
+++ Fundamentos/loopvar.js
@@ -0,0 +1,6 @@
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log(i) // como var não tem escopo de bloco ele esta visivel fora do bloco
+//no mesmo caso se fosse let isso não aconteceria
