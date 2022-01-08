3a12bb138b7f0f423ac2d928debc5fd66f229cb0
Lucas Schmidt <lucas.schmidt015@gmail.com>
2022-01-08 19:29:18 -0300

    funcoes

---
+++ Fundamentos/funcao1.js
@@ -0,0 +1,6 @@
+function imprimirSoma(a, b){
+    return console.log(a + b)
+}
+
+
+console.log(imprimirSoma(2,3))
---
+++ Fundamentos/funcao2.js
@@ -0,0 +1,15 @@
+const imprimirSoma = function (a,b){
+    return a+b
+}
+
+console.log(imprimirSoma(2,3))
+
+const soma = (a,b) =>{ //maneira diferente de definir uma função
+    return a+b
+}
+
+console.log(soma(4,7))
+
+const subtracao = (a,b) => a-b  //não precisa o return se for só uma linha de cidigo
+
+console.log(subtracao(7,1))
